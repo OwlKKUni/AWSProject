30c313303d27d94be2af6da5be8078ffac34863b
OwlKKUni <wrx75690@student.wroclaw.merito.pl>
2024-12-20 01:39:34 +0100

    added comment for linux

--- create_env_vars.py
+++ create_env_vars.py
@@ -6,3 +6,9 @@ os.system('setx AWS_RDS_USERNAME admin')
 os.system('setx AWS_RDS_PASSWORD BlueDolphin11!')
 
 print("Environment variables set successfully.")
+
+# Use this on linux in terminal - not persistent
+# export AWS_RDS_ENDPOINT=kakepa-final-mysql-db-1.c04vdpgrzemq.us-east-1.rds.amazonaws.com
+# export AWS_RDS_DATABASE=DBTest1
+# export AWS_RDS_USERNAME=admin
+# export AWS_RDS_PASSWORD=BlueDolphin11!
